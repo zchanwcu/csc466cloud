2a2ec59d2ad47dd16a09400fb2f2065a6743309c
zchanwcu <70228302+zchanwcu@users.noreply.github.com>
2020-11-05 12:55:01 -0500

    Create install_mpi.sh

---
+++ install_mpi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+sudo yum -y group install "Development Tools"
+sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
+sudo tar xzf openmpi-3.1.2.tar.gz
+cd openmpi-3.1.2
+sudo ./configure --prefix=/opt/openmpi/3.1.2
+sudo make
+sudo make all install
+
+echo "export PATH=$PATH:/opt/openmpi/3.1.2/bin" | sudo tee -a /etc/profile 
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/openmpi/3.1.2/lib/" | sudo tee -a /etc/profile 
+cd ..
+sudo rm -Rf openmpi-3.1.2
+sudo rm -Rf openmpi-3.1.2.tar.gz 
