7cab0078d034c3ec54c5650fbd8b6208419220c6
zchanwcu <70228302+zchanwcu@users.noreply.github.com>
2020-12-02 21:16:52 -0500

    Add files via upload

--- profile.py
+++ profile.py
@@ -1,20 +1,48 @@
 import geni.portal as portal
-import geni.rspec.pg as rspec
+import geni.rspec.pg as pg
+import geni.rspec.igext as IG
+
+# Create a portal context.
+pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
-# Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node.routable_control_ip = "true"
+request = pc.makeRequestRSpec()
 
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo ufw allow in "Apache Full"'))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo systemctl status apache2'))
+tourDescription = \
+"""
+This profile provides the template for a full research cluster with head node, scheduler, compute nodes, and shared file systems.
+At the moment, we start with a single node running MPI.
+"""
+
+# Setup the Tour info with the above description and instructions.  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
+for i in range(4):
+  if i == 0:
+    node = request.XenVM("head")
+    node.routable_control_ip = "true"
+  else:
+    node = request.XenVM("compute-" + str(i))
+    node.cores = 4
+    node.ram = 4096
+    
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+  
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+
+  if i == 0:
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/nfs/nfs-server.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_mpi.sh"))
+  else: 
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/nfs/nfs-client.sh"))
+
 # Print the RSpec to the enclosing page.
-portal.context.printRequestRSpec()
+pc.printRequestRSpec(request)
