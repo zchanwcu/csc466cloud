338780e1a194344c2b361000ac22a443909319aa
zchanwcu <70228302+zchanwcu@users.noreply.github.com>
2020-12-03 22:01:01 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,45 +1,15 @@
-import geni.portal as portal
-import geni.rspec.pg as pg
-import geni.rspec.igext as IG
+#!/bin/bash
+set -x
+sudo yum -y group install "Development Tools"
+sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
+sudo tar xzf openmpi-3.1.2.tar.gz
+cd openmpi-3.1.2
+sudo ./configure --prefix=/opt/openmpi/3.1.2
+sudo make
+sudo make all install
 
-# Create a portal context.
-pc = portal.Context()
-
-# Create a Request object to start building the RSpec.
-request = pc.makeRequestRSpec()
-
-tourDescription = \
-"""
-This profile provides the template for a full research cluster with head node, scheduler, compute nodes, and shared file systems.
-At the moment, we start with a single node running MPI.
-"""
-
-# Setup the Tour info with the above description and instructions.  
-tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,tourDescription)
-request.addTour(tour)
-
-prefixForIP = "192.168.1."
-link = request.LAN("lan")
-
-for i in range(3):
-  if i == 0:
-    node = request.XenVM("head")
-    node.routable_control_ip = "true"
-  else:
-    node = request.XenVM("compute-" + str(i))
-    node.cores = 4
-    node.ram = 4096
-    
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-  
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
-  
-# Print the RSpec to the enclosing page.
-pc.printRequestRSpec(request)
+echo "export PATH=$PATH:/opt/openmpi/3.1.2/bin" | sudo tee -a /etc/profile 
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/openmpi/3.1.2/lib/" | sudo tee -a /etc/profile 
+cd ..
+sudo rm -Rf openmpi-3.1.2
+sudo rm -Rf openmpi-3.1.2.tar.gz 
